57ca0d60ac51cf57942a12c6161fed447c7f772d
ivara <952312520@qq.com>
2020-03-22 20:19:56 +0800

    hah

---
+++ NewWorld/app/src/main/java/com/example/newworld/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.newworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+//        Button button1 = findViewById(R.id.button1);
+//        Button button2 = findViewById(R.id.button2);
+//
+//        final TextView tv1 = findViewById(R.id.tv1);
+//        final TextView tv2 = findViewById(R.id.tv2);
+//
+//        button1.setOnClickListener(new View.OnClickListener( ){
+//            @Override
+//            public void onClick (View v){
+//                tv1.setText("wonderful");
+//            }
+//        });
+//
+//        button2.setOnClickListener(new View.OnClickListener( ){
+//            @Override
+//            public void onClick (View v){
+//                tv1.setText("Hello World!");
+//                tv2.setText("也不难");
+//            }
+//        });
+
+    }
+
+}
---
+++ test/app/src/main/java/com/example/hw2/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.hw2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+
+        final TextView tv1 = findViewById(R.id.tv1);
+        final TextView tv2 = findViewById(R.id.tv2);
+
+        button1.setOnClickListener(new View.OnClickListener( ){
+        @Override
+        public void onClick (View v){
+                tv1.setText("wonderful");
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener( ){
+            @Override
+            public void onClick (View v){
+                tv1.setText("Hello World!");
+                tv2.setText("也不难");
+            }
+        });
+
+    }
+
+}
